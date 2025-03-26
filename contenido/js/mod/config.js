d09bc0ebbcbf1786f2fbfdfa1bfd35e371767541
foxbyte504 <juanfigeroa504@gmail.com>
2025-03-26 13:57:25 -0300

    Add files via upload

--- contenido/js/mod/config.js
+++ contenido/js/mod/config.js
@@ -2,25 +2,243 @@ export default {
     mainImage: './assets/icon2.png',  // Asegúrate de que la imagen está en 'img/'
     articles: [
         {
-            title: 'Actions and stuff',
-            imageUrl: './img/articulo1.png',  // Imagen local
-            link1: 'https://link-hub.net/1285228/acciones-and-stuff-122',
-            link2: 'https://pin.it/4axfTBiGz',
-            description: 'This is an optional description for Action and stuff.'
+            title: 'SCP Dystopia',
+            imageUrl: './assets/addon/IMG_20250215_102329_817.jpg',  // Imagen local
+            link1: 'https://link-target.net/1285228/scp-dystopia',
+            link2: '',
+            description: 'No se encotro creador.'
         },
         {
-            title: 'Artículo 2',
-            imageUrl: './img/articulo2.png',  // Imagen local
-            link1: 'https://www.ejemplo.com/articulo2_link1',
-            link2: 'https://www.ejemplo.com/articulo2_link2',
-            description: 'Optional description for Artículo 2.'
+            title: 'Epic Bosses',
+            imageUrl: './assets/addon/IMG_20250210_180400_503.jpg',  // Imagen local
+            link1: 'https://direct-link.net/1285228/epic-bosses',
+            link2: '',
+            description: 'No se encontro creador.'
         },
         {
-            title: 'Artículo 3',
-            imageUrl: './img/articulo3.png',  // Imagen local
-            link1: 'https://www.ejemplo.com/articulo3_link1',
-            link2: 'https://www.ejemplo.com/articulo3_link2',
-            description: 'Optional description for Artículo 3.'
+            title: 'Marvel superheroes',
+            imageUrl: './assets/addon/IMG_20250210_180810_504.jpg',
+            link1: 'https://direct-link.net/1285228/marvel-superheroes',
+            link2: '',
+            description: 'No se encotro creador.'
+        },
+        {
+            title: 'Weapons & Armors',
+            imageUrl: './assets/addon/WeaponsAndArmors_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/weapons-y-armors',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=548b99ff-688e-4689-a0fd-354f4e32e698',
+            description: ''
+        },
+        {
+            title: 'Bosses+',
+            imageUrl: './assets/addon/BPL_Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/bosses-plus',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=a1f41469-d937-4b58-97e8-09ebb4196fa7',
+            description: ''
+        },
+        {
+            title: 'Morph into Anything',
+            imageUrl: './assets/addon/BAM_Thumbnail_0.jpg',
+            link1: 'https://link-target.net/1285228/morph-into-anything',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=cd7208a2-3430-4200-948f-15fba35ec5e5',
+            description: ''
+        },
+        {
+            title: 'Dragons',
+            imageUrl: './assets/addon/Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/dragons',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=d790c8b7-6dcc-43c9-85fd-8ece6627ad42',
+            description: ''
+        },
+        {
+            title: 'BIome+Realims',
+            imageUrl: './assets/addon/IMG_20250210_180844_593.jpg',
+            link1: 'https://direct-link.net/1285228/biome-mas-realism',
+            link2: '',
+            description: 'No se encontro enlace del creador.'
+        },
+        {
+            title: 'Wizard Craft',
+            imageUrl: './assets/addon/wizardcraftaddon_Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/wizard-craft',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=4c68c76e-f0d4-41ef-8e4e-96f10facc972',
+            description: ''
+        },
+        {
+            title: 'DeadZone',
+            imageUrl: './assets/addon/IMG_20250217_131318_366.jpg',
+            link1: 'https://link-center.net/1285228/dead-zone',
+            link2: '',
+            description: 'No se encotro enlace del creador.'
+        },
+        {
+            title: 'Alex cave',
+            imageUrl: './assets/addon/IMG_20250216_193738_063.jpg',
+            link1: 'https://link-hub.net/1285228/alexs-cave',
+            link2: '',
+            description: 'No se encotro el creador.'
+        },
+
+        {
+            title: 'Gravestone',
+            imageUrl: './assets/addon/gravestones_Thumbnail_0.jpg',
+            link1: 'https://link-target.net/1285228/gravestone',
+            link2: 'https://www.minecraft.net/en-us/marketplace/pdp?id=da414f0f-c795-4039-b3f2-f535d5b69d95',
+            description: ''
+        },
+        {
+            title: 'Horror survival 2.0',
+            imageUrl: './assets/addon/Survival_But_Horror_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/survival-horror',
+            link2: 'https://www.minecraft.net/en-us/marketplace/pdp?id=a1da6f9b-d454-48cf-b0eb-d140020602d9',
+            description: ''
+        },
+        {
+            title: 'Dwellers',
+            imageUrl: './assets/addon/Dwellers_Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/dwellers',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=2e5049f1-cf17-4fba-b60d-d1d92284e3bc',
+            description: ''
+        },
+        {
+            title: 'Parasites 1.0.2',
+            imageUrl: './assets/addon/PARASITE_SURVIVAL__Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/parasites-add-on',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=f5bdb521-807e-4280-9335-0cbded4a7276',
+            description: ''
+        },
+        {
+            title: 'Epic helper',
+            imageUrl: './assets/addon/epichelpers_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/epic-helper',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=50ca5d66-6877-4cd7-ac24-f810f5240efb',
+            description: ''
+        },
+        {
+            title: 'Scary add-on 1.2',
+            imageUrl: './assets/addon/scary_thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/scary-add-on-12',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=eb215fbe-8b0c-4b13-bd9b-b704cf1c148d',
+            description: ''
+        },
+        {
+            title: 'Nomadic',
+            imageUrl: './assets/addon/Nomadic_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/nomadic',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=00f47889-12b4-45b3-8411-2dc667663e9d',
+            description: ''
+        },
+        {
+            title: 'Dinosaur',
+            imageUrl: './assets/addon/dinos_addon_Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/dinosaurs',
+            link2: '',
+            description: 'No se encotro al creador.'
+        },
+        {
+            title: 'Tinkers Contruct',
+            imageUrl: './assets/addon/Tinkers_Construct-Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/tinkers-construct',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=e79ada28-0939-4161-bfe4-e2ce0bf7d69c',
+            description: ''
+        },
+        {
+            title: 'Mowzie Mob',
+            imageUrl: './assets/addon/mowzie_thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/mowzies-mob',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=9afc43e7-dc2c-44c0-a3b8-7107fa9da196',
+            description: ''
+        },
+        {
+            title: 'Biomes realims 1.2',
+            imageUrl: './assets/addon/realisticbiomes_Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/realistic-biomes',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=2249c366-c812-4fe5-a6ec-abe9d6bea6bc2',
+            description: ''
+        },
+        {
+            title: 'Strcuture generation',
+            imageUrl: './assets/addon/Structure_Generation_Add-On_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/structure-generation',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=12a90ac1-06bb-4000-9abf-383ca1c1df6b',
+            description: ''
+        },
+        {
+            title: 'Better on Bedrock',
+            imageUrl: './assets/addon/betteronbedrock_Thumbnail_0.jpg',
+            link1: 'https://link-center.net/1285228/better-on-bedrock',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=6c3a6979-dc77-41c6-b19e-0071dabedf71',
+            description: ''
+        },
+        {
+            title: 'DynamicLight',
+            imageUrl: './assets/addon/DynamicLight_Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/dynamic-light',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=2c9dedd5-0918-41e5-ac58-d952a2bae7a6',
+            description: ''
+        },
+        {
+            title: 'FurrCraft',
+            imageUrl: './assets/addon/IMG_20250217_131805_144.jpg',
+            link1: 'https://link-hub.net/1285228/furrycraft',
+            link2: '',
+            description: 'No se encontro al creador.'
+        },
+        {
+            title: 'Dragons',
+            imageUrl: './assets/addon/ADDD_Thumbnail_0.jpg',
+            link1: 'https://direct-link.net/1285228/dragons-13',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=71a0edf5-9999-4cf2-83cc-48e5c1ab8f0c',
+            description: ''
+        },
+        {
+            title: 'Decoration',
+            imageUrl: './assets/addon/Decoration_Thumbnail_0.jpg',
+            link1: 'https://link-target.net/1285228/decoration1',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=27f7178c-8dfe-449c-91c3-a46f2f2c55d9',
+            description: ''
+        },
+        {
+            title: 'RedStone tech',
+            imageUrl: './assets/addon/Redstone_Tech_Thumbnail_0.jpg',
+            link1: 'https://link-target.net/1285228/redstone-tech',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=cb71f50a-edb0-4fac-b30b-d69e43f87e23',
+            description: ''
+        },
+        {
+            title: 'Fantasy ores',
+            imageUrl: './assets/addon/IMG_20250217_132134_768.jpg',
+            link1: 'https://link-hub.net/1285228/fantasy-ores',
+            link2: '',
+            description: 'No se encotro enlace del creador'
+        },
+        {
+            title: 'Trains',
+            imageUrl: './assets/addon/TA_Thumbnail_0.jpg',
+            link1: 'https://link-target.net/1285228/trains',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=d42ac193-22e7-4aaf-a6ba-21349e9f13ca',
+            description: ''
+        },
+        {
+            title: 'Combine ores',
+            imageUrl: './assets/addon/CombineOres_Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/combine-ores',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=338de759-0783-47a3-b926-bc7f0bd7837d',
+            description: ''
+        },
+        {
+            title: 'Magic add-on',
+            imageUrl: './assets/addon/IMG_20250319_164748_437.jpg',
+            link1: 'https://direct-link.net/1285228/magic-add-on',
+            link2: '',
+            description: 'No se encontro al creador.'
+        },
+        {
+            title: 'Utilities',
+            imageUrl: './assets/addon/UTILITIES_Thumbnail_0.jpg',
+            link1: 'https://link-hub.net/1285228/utilities',
+            link2: 'https://www.minecraft.net/en-us/pdp.html?id=b3f754e9-ba2a-44fe-8c03-92f171087f3e',
+            description: ''
         }
     ]
 };
